7041d5cedf7f65bb96575509aa1f7e7f31866bd9
emilekamana <emileizere@gmail.com>
2024-06-18 20:38:59 +0200

    chore: setup tailwind CSS

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View className="min-h-[85vh] flex w-full items-center justify-center">
+      <Text className="text-4xl">Todo</Text>
+    </View>
+  );
+};
+
+export default Home;
